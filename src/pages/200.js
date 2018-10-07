3d596025d7ed295880a3e8cea84b8cc2f88296c2
Michael Paterson <mike@beamery.com>
2018-10-07 17:14:13 +0100

    adding 200 page

---
+++ src/pages/200.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const FoundPage = () => (
+  <Layout>
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <h1>FOUND</h1>
+          <p>Well done, this exists</p>
+        </div>
+      </section>
+    </div>
+  </Layout>
+)
+
+export default FoundPage
